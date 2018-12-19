c45dc5a4eef09e9d70df51f0aec817b257303b6c
Jason Frey <fryguy9@gmail.com>
2018-12-19 16:17:22 -0500

    Day 04

---
+++ 04/04b.cr
@@ -0,0 +1,23 @@
+guard = sleep_start = nil
+total = Hash(Int32, Int32).new(0)
+minutes = Hash(Int32, Array(Int32)).new
+
+File.read_lines("input").sort.each do |l|
+  time = Time.parse_utc(l[1, 16], "%Y-%m-%d %H:%M")
+
+  case l[19..-1]
+  when "wakes up"
+    asleep = (time - sleep_start.not_nil!).to_i / 60
+    total[guard.not_nil!] += asleep
+    asleep.times { |m| minutes[guard.not_nil!][sleep_start.not_nil!.minute + m] += 1 }
+  when "falls asleep"
+    sleep_start = time
+  when /\AGuard #(\d+) begins shift\z/
+    guard = $1.to_i
+    minutes[guard] ||= Array(Int32).new(60, 0)
+  end
+end
+
+guard, slots = minutes.max_by { |guard, slots| slots.max }
+minute = slots.index(slots.max).not_nil!
+puts guard * minute
